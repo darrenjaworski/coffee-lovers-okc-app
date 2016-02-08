9855ce7cada394f6c0b46ef524f80f4e2befe2d6
Darren Jaworski <djaworski@staplegun.us>
2016-02-08 14:59:49 -0600

    Sort and filter v1.

--- app/home/home.js
+++ app/home/home.js
@@ -9,12 +9,37 @@ angular.module('okcCoffee.home', ['ngRoute', 'ngSanitize'])
   });
 }])
 
-.controller('HomeCtrl', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
+.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
 
   $scope.$emit('resetMap');
   $scope.$emit('zoomMap');
+  $scope.sortProp = 'name';
+  $scope.sortReverse = false;
+  $scope.filterProp = '';
   $http.get('./../assets/static-data/yelp.json').then(function(response) {
     $scope.shops = response.data;
+    $scope.shops.forEach(function(d){
+      d.qualities = [];
+      if (d.expresso) {
+        d.qualities.push('expresso')
+      }
+      if (d.speciality) {
+        d.qualities.push('speciality')
+      }
+      if (d.drip) {
+        d.qualities.push('drip')
+      }
+      if (d.roaster) {
+        d.qualities.push('roaster')
+      }
+      if (d.tea) {
+        d.qualities.push('tea')
+      }
+      if (d.food) {
+        d.qualities.push('food')
+      }
+    })
+    console.log( $scope.shops );
   });
   $http.get('./../assets/static-data/blog.json').then(function(response) {
     $scope.blogs = response.data;
@@ -22,7 +47,7 @@ angular.module('okcCoffee.home', ['ngRoute', 'ngSanitize'])
 
 }])
 
-.directive('shopLi', function(){
+.directive('shopLi', function($cookies){
   function link($scope, el, attrs){
 
     if ($scope.$parent.$last){
@@ -32,13 +57,26 @@ angular.module('okcCoffee.home', ['ngRoute', 'ngSanitize'])
 
   }
 
-  function controller($scope) {
+  function controller($scope, $cookies) {
 
     $scope.centerMap = function(id){
       $scope.$emit('centerMap', id - 1);
       $('body').toggleClass('active-map');
-    }
+    };
 
+    $scope.favorite = function(id) {
+      var favoritesArray = $cookies.get('favorites');
+      console.log(favoritesArray)
+      if ( favoritesArray.indexOf(id) !== -1 ) {
+        console.log('need to remove from array');
+        favoritesArray.splice( favoritesArray.indexOf(id) )
+        console.log(favoritesArray);
+      } else {
+        console.log('need to add to array')
+        console.log(favoritesArray)
+        favoritesArray.push(id)
+      }
+    };
   }
 
   return {
